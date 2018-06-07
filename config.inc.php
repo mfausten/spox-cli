5d791c2e11abac273a24c3fff9ed2650cb3127f4
Michael Fausten <m.fausten@anymotion.de>
2018-06-07 12:26:42 +0200

    updated xpath selectors to new site structure

--- config.inc.php
+++ config.inc.php
@@ -20,11 +20,11 @@ const TICKER_DAY_STORAGE_FILE_NAME = 'news.json';
 
 const TICKER_NEWS_FILE_PATH = TICKER_DAY_STORAGE_PATH . '/' . TICKER_DAY_STORAGE_FILE_NAME;
 
-define('TICKER_LINK_SELECTOR', '//a[contains(@href, "rundumdenball/' . date('ym') . '/' . date('dm') . '")]/@href');
+define('TICKER_LINK_SELECTOR', '//a[contains(@href, "rundumdenball/")]/@href');
 
 const TICKER_ITEMS_SELECTOR = '//div[@class="tickeritem"]';
 
-const TICKER_ITEM_ID      = '//div[@class="wrp2"]/a[@class="date"]/@id';
-const TICKER_ITEM_TIME    = '//div[@class="wrp2"]/a[@class="date"]';
+const TICKER_ITEM_ID      = '//div[@class="wrp2"]/a[@class="anc"]/@id';
+const TICKER_ITEM_TIME    = '//div[@class="wrp2"]/span[@class="date"]';
 const TICKER_ITEM_HEADING = '//div[@class="wrp3"]/h2';
-const TICKER_ITEM_NEWS    = '//div[@class="wrp3"]/div[@class="text"]/p';
+const TICKER_ITEM_NEWS    = '//div[@class="wrp3"]/div[@class="text rtx"]/p';
